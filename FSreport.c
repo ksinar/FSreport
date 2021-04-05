3f33954a9728308ef70998b69d30d1c61fea15a9
Kaan Sinar <sinarkaan@gmail.com>
2021-04-05 19:26:26 -0400

    add loop through root dir

--- FSreport.c
+++ FSreport.c
@@ -7,21 +7,35 @@ int main(int argc, char **argv){
         return -1;
     }
     */
+
+    DIR *rootdir = NULL;
+    rootdir = opendir(argv[1]);
+    if(rootdir == NULL){
+        fprintf(stderr, "could not open directory\n");
+        exit(1);
+    }
+    printf("---------------------------\n");
+    printf("Level 1 Inodes: %s\n", argv[1]);
+    struct dirent *directory = readdir(rootdir);
     int fd = 0;
-    fd = open(argv[1], O_RDONLY);
+    while(directory != NULL){
+        //printf("dirname: %s\n",directory->d_name);
 
-    struct stat filestat;
-    fstat(fd,&filestat);
 
-    printf("---------------------------\n");
-    printf("%d: ",filestat.st_ino);
-    printf("%d\t",filestat.st_size);
-    printf("%d\t",filestat.st_blocks);
-    printf("%d\t",(filestat.st_size / 512));
-    printf("%s\n", argv[1]);
-    printf("---------------------------\n");
+        fd = open(directory->d_name, O_RDONLY);
 
-    //printf("Number of Links: \t%d\n",filestat.st_nlink);
+        struct stat filestat;
+        fstat(fd,&filestat);
+
+        printf("%10d: \t",filestat.st_ino);
+        printf("%d\t",filestat.st_size);
+        printf("%d\t",filestat.st_blocks);
+        printf("%d\t",(filestat.st_size / 512));
+        printf("%s\n", directory->d_name);
+        directory = readdir(rootdir);
+    }
+        printf("---------------------------\n");
+
 /*
     printf("File Permissions: \t");
     printf( (S_ISDIR(filestat.st_mode)) ? "d" : "-");
@@ -38,6 +52,8 @@ int main(int argc, char **argv){
 
     printf("The file %s a symbolic link\n\n", (S_ISLNK(filestat.st_mode)) ? "is" : "is not");
 */
-    close(fd);
+//    close(fd);
+
+    free(rootdir);
     return 0;
 }
